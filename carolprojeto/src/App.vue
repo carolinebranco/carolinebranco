98dcde1cc57143e7f5d3c1d4bbf52eb7035b623a
caroline branco <carolinebranco1@hotmail.com>
2022-05-06 11:30:57 -0300

    qdsa

--- carolprojeto/src/App.vue
+++ carolprojeto/src/App.vue
@@ -1,14 +1,52 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Item from './components/Item.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<script>
+// import Dados from './Dados/Services.json';
+
+export default {
+  data(){
+    return{
+      servicos: null
+    }
+  },
+  mounted(){
+    let target = "https://raw.githubusercontent.com/yurimarcon/link-services/main/src/Dados/Services.json";
+    fetch(target)
+      .then(res => res.json())
+      .then(res => this.servicos = res)
+      .catch(e => console.log(e))
+  }
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo2.png
-  " />
+  <img alt="Profile" style="width:150px; border-radius:50%" src="https://avatars.githubusercontent.com/u/44410208?v=4" />
+  
+  <h1>Yuri Marcon</h1>
+  
+  <blockquote cite="Mahatma Gandhi">
+    <p>
+      "Seja a mudança que você quer ver no mundo"
+      <small> - Mahatma Gandhi - </small>
+    </p>
+  </blockquote>
+  
+  <hr/>
+  <div class="items">
+    <Item 
+    v-for="servico, index in servicos"
+    :key="index"
+    :item="servico"
+    />
 
+  </div>
 
+  <Footer />
 </template>
 
 <style>
@@ -19,5 +57,17 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 100px;
+}
+html{
+  background: url("https://cdn.pixabay.com/photo/2017/01/24/03/53/plant-2004483_960_720.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100%;
+}
+.items{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
---
+++ carolprojeto/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>2021 - Yuri Marcon</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+footer{
+  width: 100%;
+  border-top: 1px solid rgb(207, 207, 207);
+  height: 40px;
+  padding-top: 20px;
+  background-color: honeydew;
+  position: fixed;
+  bottom: 0;
+  font-weight: 900;
+}
+
+</style>
