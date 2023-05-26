1aecbea65bd6773fa6570cc037df1cac90a1a84b
caroline branco <101421521+carolinebranco@users.noreply.github.com>
2023-05-26 14:21:03 -0300

    Update vite.config.js

--- carolprojeto/vite.config.js
+++ carolprojeto/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig({]
 })
